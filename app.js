4451e58c50fc25115c66eb5485228e6b0cc05f84
kimthan <kimthan85@gmail.com>
2018-05-17 21:04:58 +0200

    v3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 3000, ()=>console.log('app running on port 3000')
 )
