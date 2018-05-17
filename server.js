a6949945a9e9cc4579dfa84bb224afc1dec6ecd8
kimthan <kimthan85@gmail.com>
2018-05-17 21:28:21 +0200

    cross

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express()
 const path = require('path')
 const data = require('./Data')
 
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    next()
+})
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/api', (req, res)=>{
+   
     res.json({data})
     console.log(JSON.stringify(data))    
 })
