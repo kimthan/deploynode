7ec8bcdf562bf404b518bcded430b32b1454d5ac
kimthan <kimthan85@gmail.com>
2018-05-17 21:38:15 +0200

    cross5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const data = require('./Data')
-const cors = require('cors')
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/api', (req, res)=>{
@@ -10,7 +10,7 @@ app.get('/api', (req, res)=>{
     res.json({data})
     console.log(JSON.stringify(data))    
 })
-app.use(cors({origin: 'http://localhost:3000'}));
+
 app.listen(process.env.PORT || 3000, ()=>console.log('app running on port 3000')
 )
 
