b879ad608de3153ce5867cc7bcaf8ca007e23c69
kimthan <kimthan85@gmail.com>
2018-05-17 21:16:13 +0200

    pics

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-
+const data = require('./Data')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
+app.get('/api', (req, res)=>{
+    res.json({data})
+    console.log(JSON.stringify(data))    
+})
 app.listen(process.env.PORT || 3000, ()=>console.log('app running on port 3000')
 )
 
